8c90e12fbeb833e40a41d6047357232831f2c3a2
jbernadas <jb3rns@gmail.com>
2020-09-09 12:04:49 -0700

    Added image upload capabilities with automated alt text, not yet tested

--- uploader.py
+++ uploader.py
@@ -59,10 +59,9 @@ help_text = """
 """
 def uploader(QUALS, TARGET_SITE):
     # Initialize webdriver. We are using Firefox because Chrome is spotty on the login bit.
-    # driver = webdriver.Firefox()
+    driver = webdriver.Firefox()
 
     # # Target base URL
-    # target_site = input("What is the name of the website? ")
     target_site = TARGET_SITE
 
     # Strips the leading 'https://' from target_site
@@ -72,11 +71,10 @@ def uploader(QUALS, TARGET_SITE):
     site_name = parsedUrl.hostname.split('-')[0]
     
     # Login to site manually
-    # driver.get(target_site + '/login')
+    driver.get(target_site + '/login')
 
     # # Ask if user has logged-in to site and ready to proceed
-    # proceed = input(
-    #     "Are you logged-in and ready to proceed? 'y' = yes, any key to abort: ")
+    proceed = input("Are you logged-in and ready to proceed? 'y' = yes, any key to abort: ")
 
     # List of file types we are looking to upload
     DOC_QUALIFIERS = [
@@ -102,43 +100,52 @@ def uploader(QUALS, TARGET_SITE):
 
     QUALIFIERS = []
 
+    # Target folder
+    folder_location = "site_report\\" + site_name
+
     # The directory where our soon-to-be uploaded documents reside
     FILESDIR = ""
 
     if QUALS.upper() == "DOC":
         QUALIFIERS = DOC_QUALIFIERS
-        FILESDIR = "site_report\\" + site_name + "\\docs_for_upload"
+        FILESDIR = folder_location + "\\docs_for_upload"
 
     if QUALS.upper() == "IMG":
         QUALIFIERS = IMG_QUALIFIERS
-        FILESDIR = "site_report\\" + site_name + "\\imgs_for_upload"
+        FILESDIR = folder_location + site_name + "\\imgs_for_upload"
     
 
     fileCount = sum([len(files) for r, d, files in os.walk(FILESDIR)])
 
     count = 0
 
+    # initialize a wait variable that makes the driver wait for so many seconds
+    wait = WebDriverWait(driver, 120)
+
+    reconstitutedAltInfo = {}
+
+    with open(os.path.join(folder_location, 'alt-info.txt')) as f:
+        reconstitutedAltInfo = json.load(f)
+
     # For each qualifier in list of QUALIFIERS
     for qualifier in QUALIFIERS:
         # for each filename inside our document directory
         for filename in os.listdir('./' + FILESDIR):
-            # and if filename ends with the qualifier being iterated
+            # and if filename ends with the qualifier being iterated and it contains a .PDF file
             if filename.endswith(qualifier) and '.pdf' in QUALIFIERS:
-                # initialize a wait variable that makes the driver wait for so many seconds
-                wait = WebDriverWait(driver, 120)
-                # go to the /media/add/document of the Drupal site
+                # Go to the /media/add/document of the Drupal site
                 driver.get(target_site + "/media/add/document")
-                # look for the id of input area and fill it with the path to our file-to-upload
+                # Look for the id of input area and fill it with the path to our file-to-upload
                 driver.find_element_by_id(
                     "edit-field-document-0-upload").send_keys(os.getcwd() + '\\' + FILESDIR + '\\' + filename)
-                # wait until the page is finished uploading, in this case 
+                # Wait until the page is finished uploading, in this case \ 
                 # when the remove button appears, before proceeding
                 wait.until(presence_of_element_located(
                     (By.NAME, 'field_document_0_remove_button')))
-                # look for the 'name' input box and fill it with the same name as the file
+                # Looks for the 'name' input box and fill it with the same name as the file
                 driver.find_element(
                     By.ID, "edit-name-0-value").send_keys(filename)
-                # wait for the 'save' button to appear, then click it.
+                # Wait for the 'save' button to appear, then robot clicks it.
                 wait.until(element_to_be_clickable(
                     (By.XPATH, 'html/body/div[2]/div[1]/main/div[4]/div[1]/form/div[8]/input[@id="edit-submit"]'))).click()
                 # rinse, repeat.
@@ -148,12 +155,52 @@ def uploader(QUALS, TARGET_SITE):
                 continue
 
             if filename.endswith(qualifier) and '.jpg' in QUALIFIERS:
- 
-    
-    # print('Upload complete! {} of {} documents uploaded.'.format(count, (fileCount)))
-    # print('\a')
-    # # Exit the driver.
-    # driver.quit()
+                print(filename)
+                driver.get(target_site + "/media/add/image")
+                driver.find_element_by_id("edit-image-0-upload").send_keys(os.getcwd() + '\\' + FILESDIR + '\\' + filename)
+                wait.until(presence_of_element_located((By.NAME, 'image_0_remove_button')))
+
+                ### ALTERNATIVE TEXT HANDLER ###
+                ##  Uncomment the below options to control how alt text for each image is populated
+
+                ##  Manual  ## 
+                # user_inputted_alt_text = input("What would you like to put as 'alt' text for {}? ".format(filename))
+                # alt_text = user_inputted_alt_text
+
+                ##  Filename Based ##
+                # strip_extension = filename.replace(qualifier, "")
+                # underscore_to_space = strip_extension.replace("_", " ")
+                # alt_text = underscore_to_space.replace("-", " ")
+
+                ##  Automated  ##
+                ## if filename has corresponding alt text use it
+                if reconstitutedAltInfo.get(filename):
+                    alt_text = reconstitutedAltInfo
+                ## otherwise
+                else:
+                    ## use filename as alt text
+                    strip_extension = filename.replace(qualifier, "")
+                    underscore_to_space = strip_extension.replace("_", " ")
+                    alt_text = underscore_to_space.replace("-", " ")
+                
+                ## Name field cleaner
+                final_name = filename.replace("_", " ")
+                final_name = final_name.replace("-", " ")
+
+                ## Fill in the alt text input field
+                driver.find_element_by_name("image[0][alt]").send_keys(alt_text)
+
+                ## Fill in the name input field
+                driver.find_element(By.ID, "edit-name-0-value").send_keys(final_name)
+                
+                ## Wait until button is clickable, then click it
+                wait.until(element_to_be_clickable((By.XPATH, 'html/body/div[2]/div[1]/main/div[4]/div[1]/form/div[10]/input[@id="edit-submit"]'))).click()
+                continue
+
+    print('Upload complete! {} of {} documents uploaded.'.format(count, (fileCount)))
+    print('\a')
+    # Exit the driver.
+    driver.quit()
 
 def main():
     try:
