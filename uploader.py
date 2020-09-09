c215732ec394cf386aabf5aac05d02ce1ae4d381
jbernadas <jb3rns@gmail.com>
2020-09-09 09:45:31 -0700

    Uploader now printing correct file_types depending on what qualifier is in argument

--- uploader.py
+++ uploader.py
@@ -1,7 +1,23 @@
+#! /usr/bin/env python3
+"""
+    Copyright (C) 2020 Joseph Bernadas
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+"""
+
 # This is a document uploader script.
 # You have to have the same version-as-your-browser
 # Firefox GeckoDriver to use this. You have to install
-# those separately from pip, and needs to be added to PATH.
+# that separately from pip, and needs to be added to PATH.
 
 import getopt
 import os
@@ -17,57 +33,37 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.expected_conditions import presence_of_element_located
 from selenium.webdriver.support.expected_conditions import element_to_be_clickable
 
-help_text = """uploader.py version 0.0.1 (2020-09-08)
-    This custom script uploads files to a target Drupal website.
+help_text = """
+    ####################################################
+    ####   UPLOADER.py version 0.0.1 (2020-09-08)   ####
+    ####################################################
+
+    This command line program uploads files to a target Drupal website.
     
     Command line syntax:
 
-    python uploader.py <options> <URL you are uploading to>
+    python uploader.py <options> <URL you are uploading to> 
 
     Available options:
-    -h         --help                Print this text and exit
+    -h         --help                       Print this text and exit
 
-    -b <ext>   --block <ext>         Exclude URLs with the given extension;
-                                    <ext> must be without the leading dot.
-                                    The comparison is case insensitive, so
-                                    for example DOC and doc are treated
-                                    the same. You can use this option several
-                                    times to block several extensions.
-
-    -c <value> --changefreq <value>  Set the change frequency. The given value
-                                    is used in all sitemap entries (maybe a
-                                    future version of this script will change
-                                    that). The allowed values are: always,
-                                    hourly, daily, weekly, monthly, yearly,
-                                    never.
-
-    -p <prio>  --priority <prio>     Set the priority. The value must be from
-                                    the interval between 0.0 and 1.0. The value
-                                    will be used in all sitemap entries.
-
-    -m <value> --max-urls <value>    Set the maximum number of URLs to be crawled.
-                                    The default value is 1000 and the largest
-                                    value that you can set is 50000 (the script
-                                    generates only a single sitemap file).
-
-    -r <value> --ratelimit <value>   Set a crawl rate limit [requests / second],
-                                    zero (the default) results in no crawl rate
-                                    limitation.
-
-    -o <file>  --output-file <file>  Set the name of the geneated sitemap file.
-                                    The default file name is sitemap.xml.
+    -t <doc or img> --type <doc or img>     Type of files you want to upload;
+                                            choices are either 'doc' or 'img' (without the quotes).
+                                            The case is insensitive, so for example DOC and doc are treated
+                                            the same. You can use this option once.
 
     Usage example:
 
     python3 uploader.py -t doc http://target-prod.llnl.gov
 
 """
-def uploader():
+def uploader(QUALS, TARGET_SITE):
     # Initialize webdriver. We are using Firefox because Chrome is spotty on the login bit.
-    driver = webdriver.Firefox()
+    # driver = webdriver.Firefox()
 
-    # Target base URL
-    target_site = input("What is the name of the website? ")
+    # # Target base URL
+    # target_site = input("What is the name of the website? ")
+    target_site = TARGET_SITE
 
     # Strips the leading 'https://' from target_site
     parsedUrl = urlparse(target_site)
@@ -76,28 +72,58 @@ def uploader():
     site_name = parsedUrl.hostname.split('-')[0]
     
     # Login to site manually
-    driver.get(target_site + '/login')
+    # driver.get(target_site + '/login')
 
-    # Ask if user has logged-in to site and ready to proceed
-    proceed = input(
-        "Are you logged-in and ready to proceed? 'y' = yes, any key to abort: ")
+    # # Ask if user has logged-in to site and ready to proceed
+    # proceed = input(
+    #     "Are you logged-in and ready to proceed? 'y' = yes, any key to abort: ")
+
+    # List of file types we are looking to upload
+    DOC_QUALIFIERS = [
+        '.pdf',
+        '.docx',
+        '.txt',
+        '.doc',
+        '.wrf',
+        '.xls',
+        '.xlsx'
+        # '.tar',
+        # '.tgz',
+        # '.gz',
+        # '.bz2'
+    ]
+
+    IMG_QUALIFIERS = [
+        '.jpg',
+        '.jpeg',
+        '.png',
+        '.gif'
+    ]
+
+    QUALIFIERS = []
 
     # The directory where our soon-to-be uploaded documents reside
-    FILESDIR = "site_report\\" + site_name + "\\docs_for_upload"
+    FILESDIR = ""
+
+    if QUALS.upper() == "DOC":
+        QUALIFIERS = DOC_QUALIFIERS
+        FILESDIR = "site_report\\" + site_name + "\\docs_for_upload"
+
+    if QUALS.upper() == "IMG":
+        QUALIFIERS = IMG_QUALIFIERS
+        FILESDIR = "site_report\\" + site_name + "\\imgs_for_upload"
+    
 
     fileCount = sum([len(files) for r, d, files in os.walk(FILESDIR)])
 
-    # List of file types we are looking to upload
-    
     count = 0
-    ### Our uploader script ###
 
     # For each qualifier in list of QUALIFIERS
     for qualifier in QUALIFIERS:
         # for each filename inside our document directory
         for filename in os.listdir('./' + FILESDIR):
             # and if filename ends with the qualifier being iterated
-            if filename.endswith(qualifier):
+            if filename.endswith(qualifier) and '.pdf' in QUALIFIERS:
                 # initialize a wait variable that makes the driver wait for so many seconds
                 wait = WebDriverWait(driver, 120)
                 # go to the /media/add/document of the Drupal site
@@ -120,55 +146,44 @@ def uploader():
 
                 print("Upload progress: {}/{}".format(count, (fileCount)))
                 continue
+
+            if filename.endswith(qualifier) and '.jpg' in QUALIFIERS:
+ 
     
-    print('Upload complete! {} of {} documents uploaded.'.format(count, (fileCount)))
-    print('\a')
-    # Exit the driver.
-    driver.quit()
+    # print('Upload complete! {} of {} documents uploaded.'.format(count, (fileCount)))
+    # print('\a')
+    # # Exit the driver.
+    # driver.quit()
 
 def main():
     try:
         opts, args = getopt.getopt(sys.argv[1:], "ht", ["help", "type="])
+
     except getopt.GetoptError:
         sys.stderr.write(help_text)
         return 1
 
-    DOC_QUALIFIERS = [
-        '.pdf',
-        '.docx',
-        '.txt',
-        '.doc',
-        '.wrf',
-        '.xls',
-        '.xlsx'
-        # '.tar',
-        # '.tgz',
-        # '.gz',
-        # '.bz2'
-    ]
-
-    IMG_QUALIFIERS = [
-
-    ]
+    QUALS = ""
 
     for opt, arg in opts:
         if opt in ("-h", "--help"):
             sys.stderr.write(help_text)
             return 1
         elif opt in ("-t", "type="):
-            fileType = arg
-            if fileType == (arg.upper() == "DOC"):
-                fileType = DOC_QUALIFIERS
-            if fileType == (arg.upper() == "IMG"):
-                fileType = IMG_QUALIFIERS
-            if fileType in ("", ".", ".."):
-                sys.stderr.write("Please provide a sensible file name.\n")
+            QUALS = arg
+            if QUALS == (arg.upper() == "DOC"):
+                QUALS = DOC_QUALIFIERS
+            if QUALS == (arg.upper() == "IMG"):
+                QUALS = IMG_QUALIFIERS
+            if QUALS in ("", ".", ".."):
+                sys.stderr.write("Please provide a qualifier type. Choices are 'doc' or 'img'.\n")
                 return 1
     if not args:
         sys.stderr.write("You must provide the target URL to upload to.")
+        return 1
 
     ## Fire up the uploader
-    uploader()
+    uploader(args[0], args[1])
 
 if __name__ == "__main__":
     try:
